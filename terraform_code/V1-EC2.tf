4b5d04ac5c1a42e8b61633a4789072769c750a16
satendra <satendrakushwaha713@gmail.com>
2024-01-26 18:54:13 +0530

    commiting v1-ec2.tf

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,11 @@
-provider "aws" {
-  region = "us-east-1"
+ provider "aws" {
+  region = "ap-southeast-1"
+
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
-    instance_type = "t2.micro"
-    key_name = "dpp"
+  ami           = "ami-09eb2ed0e9c2f6126"
+  instance_type = "t2.micro"
+  key_name      = "dpp"
+
 }
