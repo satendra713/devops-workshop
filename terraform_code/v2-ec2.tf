44b18968dab78d0ab51fa97e2446e17f31ef872f
satendra <satendrakushwaha713@gmail.com>
2024-01-26 19:07:18 +0530

    adding v2 version

---
+++ terraform_code/v2-ec2.tf
@@ -0,0 +1,61 @@
+ provider "aws" {
+  region = "ap-southeast-1"
+
+}
+
+resource "aws_instance" "demo-server" {
+  ami           = "ami-09eb2ed0e9c2f6126"
+  instance_type = "t2.micro"
+  key_name      = "dpp"
+  security_groups = [ "demo-sg" ]
+
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH-Access"
+
+
+    ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description = "jenkins access"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
+}
